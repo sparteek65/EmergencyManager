8a52ad993cb1f8b16c3f0c0dc89047952d585ec2
unknown <sparteek65@gmail.com>
2021-11-20 14:15:09 +0530

    Added search input and toggler button with animation

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import logo from "../../assets/logo.svg";
 import search from "../../assets/search.png";
 import profile from "../../assets/profile.png";
 import "./header.css";
 
 function EMheader(props) {
+  const [searchOn, setsearchOn] = useState(false);
+  const searchInput = useRef("");
+
+  const searchToggler = () => {
+    if (searchOn === false) {
+      setsearchOn(true);
+      // searchInput.current.focus
+    } else {
+      setsearchOn(false);
+    }
+  };
+
   return (
     <div className="outer-container">
       <div className="header-container">
-        <img className="header-search" src={search} height="35px" />
+        <div onClick={searchToggler}>
+          <img className="header-search" src={search} height="35px" />
+        </div>
         <img className="header-logo" src={logo} />
         <img className="header-user" src={profile} height="35px" />
       </div>
-      <div className="search-container"></div>
+      {searchOn && (
+        <div className="search-container">
+          <input
+            autoFocus
+            ref={searchInput}
+            className="search-input"
+            type="text"
+            id="search"
+            name="search"
+            placeholder="Search . . ."
+          />
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/components/menu/menuBar.jsx
+++ frontend/src/components/menu/menuBar.jsx
@@ -1,57 +1,22 @@
 import React from "react";
 import EMmenuitem from "./menuItem";
+import home from "../../assets/2544056.png";
 
 function MenuBar(props) {
   return (
     <div className="menuBar-container">
-      <EMmenuitem
-        name="home"
-        icon="https://cdn-icons.flaticon.com/png/512/2544/premium/2544056.png?token=exp=1637381367~hmac=5e9809ea1df4d226ceb2d406e729f131"
-      />
-      <EMmenuitem
-        name="home"
-        icon="https://cdn-icons.flaticon.com/png/512/2544/premium/2544056.png?token=exp=1637381367~hmac=5e9809ea1df4d226ceb2d406e729f131"
-      />
-      <EMmenuitem
-        name="home"
-        icon="https://cdn-icons.flaticon.com/png/512/2544/premium/2544056.png?token=exp=1637381367~hmac=5e9809ea1df4d226ceb2d406e729f131"
-      />
-      <EMmenuitem
-        name="home"
-        icon="https://cdn-icons.flaticon.com/png/512/2544/premium/2544056.png?token=exp=1637381367~hmac=5e9809ea1df4d226ceb2d406e729f131"
-      />
-      <EMmenuitem
-        name="home"
-        icon="https://cdn-icons.flaticon.com/png/512/2544/premium/2544056.png?token=exp=1637381367~hmac=5e9809ea1df4d226ceb2d406e729f131"
-      />
-      <EMmenuitem
-        name="home"
-        icon="https://cdn-icons.flaticon.com/png/512/2544/premium/2544056.png?token=exp=1637381367~hmac=5e9809ea1df4d226ceb2d406e729f131"
-      />{" "}
-      <EMmenuitem
-        name="home"
-        icon="https://cdn-icons.flaticon.com/png/512/2544/premium/2544056.png?token=exp=1637381367~hmac=5e9809ea1df4d226ceb2d406e729f131"
-      />
-      <EMmenuitem
-        name="home"
-        icon="https://cdn-icons.flaticon.com/png/512/2544/premium/2544056.png?token=exp=1637381367~hmac=5e9809ea1df4d226ceb2d406e729f131"
-      />{" "}
-      <EMmenuitem
-        name="home"
-        icon="https://cdn-icons.flaticon.com/png/512/2544/premium/2544056.png?token=exp=1637381367~hmac=5e9809ea1df4d226ceb2d406e729f131"
-      />
-      <EMmenuitem
-        name="home"
-        icon="https://cdn-icons.flaticon.com/png/512/2544/premium/2544056.png?token=exp=1637381367~hmac=5e9809ea1df4d226ceb2d406e729f131"
-      />{" "}
-      <EMmenuitem
-        name="home"
-        icon="https://cdn-icons.flaticon.com/png/512/2544/premium/2544056.png?token=exp=1637381367~hmac=5e9809ea1df4d226ceb2d406e729f131"
-      />
-      <EMmenuitem
-        name="home"
-        icon="https://cdn-icons.flaticon.com/png/512/2544/premium/2544056.png?token=exp=1637381367~hmac=5e9809ea1df4d226ceb2d406e729f131"
-      />
+      <EMmenuitem name="home" icon={home} />
+      <EMmenuitem name="home" icon={home} />
+      <EMmenuitem name="home" icon={home} />
+      <EMmenuitem name="home" icon={home} />
+      <EMmenuitem name="home" icon={home} />
+      <EMmenuitem name="home" icon={home} />
+      <EMmenuitem name="home" icon={home} />
+      <EMmenuitem name="home" icon={home} />
+      <EMmenuitem name="home" icon={home} />
+      <EMmenuitem name="home" icon={home} />
+      <EMmenuitem name="home" icon={home} />
+      <EMmenuitem name="home" icon={home} />
     </div>
   );
 }
